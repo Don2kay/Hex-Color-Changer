90ac03b21e468617c4802e317d286a752e176727
Adeyemo Kabir <100203416+Don2kay@users.noreply.github.com>
2022-03-30 01:56:53 +0800

    Add files via upload

---
+++ scriptHex.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector(".hexBtn")
+
+btn.addEventListener("click",function() {
+    var hexNumber = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+
+    var hexCode = "";
+
+    for (var i = 0; i < 6; i++){
+        var randomIndex = Math.floor(Math.random() * hexNumber.length)
+        hexCode += hexNumber[randomIndex]
+    }
+document.getElementById("hex").innerHTML = hexCode
+document.body.style.backgroundColor = '#' + hexCode;
+}) 
